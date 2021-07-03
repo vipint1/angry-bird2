8f24f4a6e851f594f883fc1a08183b6911010c51
vipint1 <81351539+vipint1@users.noreply.github.com>
2021-07-03 14:53:13 +0530

    Add files via upload

---
+++ angryBirdsStage2TeacherActivity-master/Bird.js
@@ -0,0 +1,13 @@
+class Bird extends Baseclass{
+    constructor(x, y) {
+      super(x,y,50,50);
+      this.image = loadImage("sprites/bird.png");
+
+    };
+    display(){
+      super.display();
+      this.body.position.x = mouseX
+      this.body.position.y = mouseY
+    }
+  };
+  
